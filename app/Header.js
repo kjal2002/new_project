a50585dc7f9ad59f5b76fc93a9e858cde4635b58
swari <swari21@navgurukul.org>
2024-01-29 21:05:01 +0530

    responsive UI

--- app/Header.js
+++ app/Header.js
@@ -1,9 +1,25 @@
 const Header=() =>{
   return (
-    <div className="header">
-        <div className="text">NewA<img src="logo.png"/>Org</div>
+  <>
+    <div>
+        <div className="contanier-fluid text-center">NewAPI<img src="logo.png"/>Org</div>
     </div>
-  );
+    <hr/>
+    <hr/>
+    
+    <div className="d-flex justify-content-center align-item-center" >
+            <input className="search-bar" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Go</button>
+     </div>
+    
+
+
+    
+  
+  
+  
+  </>
+   )
 }
 
 export default Header
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header.js';
+import News from "./ToNews.js"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
         <div>
             <Header />
+            <News />
         </div>    
     </>
   )
