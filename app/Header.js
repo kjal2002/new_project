e3b1470fa7d3d691efcf10d2d45460b260ca4040
swari <swari21@navgurukul.org>
2024-01-29 20:29:48 +0530

    app flies folders

---
+++ app/Header.js
@@ -0,0 +1,9 @@
+const Header=() =>{
+  return (
+    <div className="header">
+        <div className="text">NewA<img src="logo.png"/>Org</div>
+    </div>
+  );
+}
+
+export default Header
---
+++ app/News.js
@@ -0,0 +1,20 @@
+import React from "react";
+function News() {
+    return (
+      <>
+        <h1>News reporting</h1>
+        <img
+          src="https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM="
+          alt="News Image"
+          style={{ maxWidth: '60%', height: 'auto' }}
+        />
+        <p>
+Newspaper Paragraph 5 (300 Words)
+
+A newspaper is an important publication that is published for the people. It carries all the latest news and views on various issues happening around us. Stories and reports published in newspapers are of news items that are local, national as also global.</p>
+      </>
+    );
+  }
+  
+  export default News;
+  
